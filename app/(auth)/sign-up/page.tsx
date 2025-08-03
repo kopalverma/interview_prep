290092413483249b2ce84e14f397c3ddbfde6209
Kopal Verma <kopal.23bce10222@vitbhopal.ac.in>
2025-08-03 22:03:29 +0530

    setup app , file and folder structure , routing and auth pages

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+import { Layout } from "lucide-react";
+// import React from "react";
+import AuthForm from "@/components/AuthForm";
+const Page = () => {
+    return <AuthForm type="sign-up" />
+    
+}
+export default Page
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,7 @@
+import { Children, ReactNode } from "react";
+const RootLayout = ({children}:{children:ReactNode}) => {
+    return(
+        <div>{children}</div>
+    )
+}
+export default RootLayout
